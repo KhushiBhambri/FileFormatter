23edc1fc62b03397cf5a3068158bf87d4aa02e75
Khushi <bhambrikhushi4@gmail.com>
2023-05-05 01:40:45 +0530

    API Endpoints Created

---
+++ src/main/java/com/kaybee/FileFormatter/FileFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.kaybee.FileFormatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFormatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaybee/FileFormatter/FileFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaybee.FileFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
